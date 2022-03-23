18015005878919bf2f8317a6eaf4295ae0ef7474
SalomonMartz <mtzelisalomon@gmail.com>
2022-03-22 18:12:20 -0600

    Add gallery

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ function App() {
       <div class="row d-flex">
 
         <div class="col-lg-4 col-md-12 mb-3">
-          <img src={require('./Images/1.JPG')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/4.jpg')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/9.jpg')} class="img-fluid mb-4" alt=""/>
+          <img src={require('./Images/1.JPG')} class="img-fluid mb-4" alt="img1"/>
+          <img src={require('./Images/4.jpg')} class="img-fluid mb-4" alt="img2"/>
+          <img src={require('./Images/9.jpg')} class="img-fluid mb-4" alt="img3"/>
           
         </div>
 
         <div class="col-lg-4 col-md-6 mb-3">
-          <img src={require('./Images/2.jpg')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/5.jpg')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/3.JPG')} class="img-fluid mb-4" alt=""/>
+          <img src={require('./Images/2.jpg')} class="img-fluid mb-4" alt="img4"/>
+          <img src={require('./Images/5.jpg')} class="img-fluid mb-4" alt="img5"/>
+          <img src={require('./Images/3.JPG')} class="img-fluid mb-4" alt="img6"/>
         </div>
 
         <div class="col-lg-4 col-md-6 mb-3">
-          <img src={require('./Images/6.jpg')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/8.jpg')} class="img-fluid mb-4" alt=""/>
-          <img src={require('./Images/7.JPG')} class="img-fluid mb-4" alt=""/>
+          <img src={require('./Images/6.jpg')} class="img-fluid mb-4" alt="img7"/>
+          <img src={require('./Images/8.jpg')} class="img-fluid mb-4" alt="img8"/>
+          <img src={require('./Images/7.JPG')} class="img-fluid mb-4" alt="img9"/>
         </div>
 
       </div>
--- src/Gallery.js
+++ src/Gallery.js
@@ -7,34 +7,61 @@ function Gallery() {
       <div className="heading text-center mb-4">
         <h2 className='font-link'>Grand Prix Formula E Mexico City</h2>
       </div>
-      <div className="row">
+      <div className="row d-flex" id='gallery1'>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/1.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/1.jpg')} alt="car1"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/2.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/2.jpg')} alt="car2"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/3.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/3.jpg')} alt="car3"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/4.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/4.jpg')} alt="car4"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/5.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/5.jpg')} alt="car5"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/6.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/6.jpg')} alt="car6"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/7.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/7.jpg')} alt="car7"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/8.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/8.jpg')} alt="car8"/>
         </div>
         <div className="col-md-6 col-lg-4 item">
-          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/9.jpg')}/>
+          <img className="img-fluid image scale-on-hover" src={require('./Gallery Fe/9.jpg')} alt="car9"/>
         </div>
+      </div>
+
+
+      <div className="heading text-center mb-4">
+        <h2 className='font-link'>Film Street Photography</h2>
+      </div>
+      <div class="row d-flex" id='gallery2'>
+
+        <div class="col-lg-4 col-md-12 mb-3">
+          <img src={require('./Film/1.jpg')} class="img-fluid mb-4" alt="img1"/>
+          <img src={require('./Film/4.jpg')} class="img-fluid mb-4" alt="img2"/>
+          <img src={require('./Film/9.jpg')} class="img-fluid mb-4" alt="img3"/>
+          
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-3">
+          <img src={require('./Film/2.jpg')} class="img-fluid mb-4" alt="img4"/>
+          <img src={require('./Film/5.jpg')} class="img-fluid mb-4" alt="img5"/>
+          <img src={require('./Film/3.jpg')} class="img-fluid mb-4" alt="img6"/>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-3">
+          <img src={require('./Film/6.jpg')} class="img-fluid mb-4" alt="img7"/>
+          <img src={require('./Film/8.jpg')} class="img-fluid mb-4" alt="img8"/>
+          <img src={require('./Film/7.jpg')} class="img-fluid mb-4" alt="img9"/>
+        </div>
+
       </div>
     </div>
 
